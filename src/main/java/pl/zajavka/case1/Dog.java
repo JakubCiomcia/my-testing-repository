ef7510e8e6b229178c2977cbf1b6a6e6ba59de53
Jakub Ciomcia <motory3@vp.pl>
2026-01-11 11:18:30 +0100

    Modyfication 3

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -2,8 +2,10 @@ package pl.zajavka.case1;
 
 public class Dog extends Animal {
 
-    @Override
-    public String whatIsYourName() {
-        return "My name is Dog";
+    private String name;
+
+    public void sniff() {
+        System.out.println("I'm soo sniffing!");
+
     }
 }
