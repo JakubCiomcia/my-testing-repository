06488138cc10634b2446760eeb52901b2526d570
Jakub Ciomcia <motory3@vp.pl>
2026-01-11 12:25:04 +0100

    Person class modification 2

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -4,15 +4,23 @@ import java.math.BigDecimal;
 
 record Person(String name, BigDecimal salary) {
 
-    public BigDecimal howMuchDoYouEarn() {
-        return salary;
+public BigDecimal howMuchDoYouEarn() {
+    return salary;
+}
+
+    public String whatIsYourJob() {
+        return "Software developer!";
     }
 
-    public Boolean doYouLikePizza() {
-        return true;
+    public String whatDoYouLike() {
+        return "Strawberries";
     }
 
     public Boolean doYouHaveABike() {
+        return true;
+    }
+
+    public Boolean doYouLikePizza() {
         return true;
     }
 }
